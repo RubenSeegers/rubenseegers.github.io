ef6f1893328de7b69e208b8abe425f6d13aa62c6
RubenSeegers <seegersruben@gmail.com>
2025-07-02 23:05:55 +0200

    Make initial header and footer

---
+++ JS/header.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("main-header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    // At the top of the page, show the header
+    header.classList.remove("hidden");
+  } else {
+    // Scrolling down or up anywhere below the top, hide the header
+    header.classList.add("hidden");
+  }
+});
