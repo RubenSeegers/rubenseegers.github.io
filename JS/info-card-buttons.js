6cb93c6a18328b3f25f724c4aadfcb37775f4b22
RubenSeegers <seegersruben@gmail.com>
2025-07-23 00:23:15 +0200

    Introduced info card, over now contains info cards instead of about cards

---
+++ JS/info-card-buttons.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.toggler').forEach(button => {
+        button.addEventListener('click', () => {
+            const infoCard = button.closest('.info-card');
+            const content = infoCard.querySelector('.content-container');
+            const bannerwrapper = infoCard.querySelector('.banner-wrapper');
+            const bannerH1 = bannerwrapper.querySelector('h1');
+            const pointer = infoCard.querySelector('.pointer');
+            infoCard.classList.toggle('expanded');
+            button.classList.toggle('used');
+            pointer.classList.toggle('visible');
+
+            setTimeout(function(){
+                content.classList.toggle('visible');
+                bannerwrapper.classList.toggle('visible');
+            }, 600);
+
+            setTimeout(function(){
+                bannerH1.classList.toggle('visible');
+            }, 1000);
+            
+        });
+    });
