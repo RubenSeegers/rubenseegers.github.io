d6551838eab5245f92f17cca5e6f0adc46d7500d
RubenSeegers <seegersruben@gmail.com>
2025-07-23 22:08:50 +0200

    Text container gets grow animation on mobile.

--- JS/info-card-buttons.js
+++ JS/info-card-buttons.js
@@ -5,6 +5,8 @@ document.querySelectorAll('.toggler').forEach(button => {
             const bannerwrapper = infoCard.querySelector('.banner-wrapper');
             const bannerH1 = bannerwrapper.querySelector('h1');
             const pointer = infoCard.querySelector('.pointer');
+            const textcontainer = infoCard.querySelector('.text-container');
+
             infoCard.classList.toggle('expanded');
             button.classList.toggle('used');
             pointer.classList.toggle('visible');
@@ -18,5 +20,8 @@ document.querySelectorAll('.toggler').forEach(button => {
                 bannerH1.classList.toggle('visible');
             }, 1000);
             
+            setTimeout(function(){
+                textcontainer.classList.toggle('grow');
+            }, 1800);
         });
     });
